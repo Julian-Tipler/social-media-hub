bdb673f1067d3c2ffa7865c4d25d5cc6f44632ab
Julian <tipler.julian@gmail.com>
2023-07-11 22:25:31 -0500

    styling additions

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { colors } from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  transparent: "transparent",
+  black: "#000",
+  white: "#fff",
+  gray: {
+    50: "#f7fafc",
+    // ...
+    900: "#171923",
+  },
+  // ...
+};
