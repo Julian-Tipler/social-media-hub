706bd8d47e624762fa418d97404adaf4318aabc6
Julian <tipler.julian@gmail.com>
2023-07-18 21:30:57 -0500

    removed unused code

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,9 +3,6 @@ import Welcome from "./Welcome";
 import { Meta } from "./Meta";
 
 export const Home = () => {
-  // FB.getLoginStatus(function (response: any) {
-  //   console.log(response);
-  // });
   return (
     <Grid
       h="800px"
